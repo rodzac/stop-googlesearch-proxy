55ae6e22093c7164572c3cbbb86cbc3c2b90d49d
ROd Zaccara <rodzac@gmail.com>
2022-08-02 14:48:39 -0300

    feat: removing 'data-' attributes on links

---
+++ core.js
@@ -0,0 +1,11 @@
+document
+    .querySelectorAll('a')
+    .forEach(element => { 
+        element
+            .getAttributeNames()
+            .forEach(attributeName => { 
+                if (attributeName.indexOf("data-") >= 0) {
+                    element.removeAttribute(attributeName)
+                }
+            }) 
+    })
